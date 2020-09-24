d8feadf19248ba9ab5b8eb9c5d3da0c91969068e
Eduard Bopp <eduard.bopp@aepsil0n.de>
2020-09-24 16:57:21 +0200

    Package server executable

--- flake.nix
+++ flake.nix
@@ -1,11 +1,54 @@
 {
-  description = "Flake for Offen, a fair web analytics tool";
+  description = "Offen, a fair web analytics tool";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.offen = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.offen;
-
+  inputs.nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    ref = "nixos-20.09";
   };
+
+  inputs.offen-src = {
+    type = "github";
+    owner = "offen";
+    repo = "offen";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, offen-src, ... }:
+
+    let
+
+      # Generate a user-friendly version numer.
+      version = builtins.substring 0 8 offen-src.lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+      overlay = final: prev:
+        with final;
+        {
+          offen = buildGoModule {
+            pname = "offen";
+            inherit version;
+            src = "${offen-src}/server";
+            vendorSha256 = "0phki0cvagd32rsz0pwbgpdgf7vblj515hw20r62g3mfq2qrwlm4";
+          };
+        };
+
+      packages = forAllSystems (system: {
+        offen = nixpkgsFor.${system}.offen;
+      });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.offen);
+    };
 }
