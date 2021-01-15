c61f8d503b6d040a5f9b59da74427630aed56a01
Eduard Bopp <eduard.bopp@aepsil0n.de>
2021-01-15 19:23:07 +0100

    Check presence of sqlite database

--- checks/offen.nix
+++ checks/offen.nix
@@ -28,6 +28,9 @@
         client.sleep(1)
         client.require_unit_state("offen.service", "active")
 
+        # Check presence of sqlite database
+        client.wait_for_file("/var/lib/offen/db.sqlite")
+
         # Test HTTP responses
         client.wait_for_open_port(8080)
         client.succeed("curl http://localhost:8080")
